5d4d6beb66b54bbae53972a092d97232474c60a9
Julien Mauroux <mioroxis@gmail.com>
2023-07-21 20:02:13 +0300

    calibration
    add calibration

--- src/CSharpMotionEx/Model/Motor.cs
+++ src/CSharpMotionEx/Model/Motor.cs
@@ -1,13 +1,17 @@
 ﻿using CSharpMotionEx.Class;
+using Microsoft.Xaml.Behaviors;
 using sFndCLIWrapper;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using ViewModel;
 namespace Model
 {
     class Motor
     {
+        private int itteration2 = 10000;
+        private List<double> m_torquescalibrationList; 
         private List<double> m_torquesList;
         private List<double> m_velocitiesList;
         private List<double> m_positionsList;
@@ -18,8 +22,14 @@ namespace Model
         private cliValueDouble m_positionValue;
 
         private cliSysMgr m_myMgr;
+
+        //Donne le couple moyen du moteur
         public double TorqueAverage { get { return m_torquesList.Average(); ; } }
+
+        // donne la vitesse moyenne du moteur
         public double VelocityAverage { get { return m_velocitiesList.Average(); ; } }
+
+        //donne la position du moteur
         public double PositionAverage { get { return m_positionsList.Average(); } }
 
         private Node m_Node;
@@ -53,6 +63,8 @@ namespace Model
             m_NodePort = portNumber;
         }
 
+
+        //permet d'attendre
         public void Wait(int delay)
         {
             m_myMgr.Delay(10);
@@ -66,6 +78,7 @@ namespace Model
             }
         }
 
+        //parametre la vitesse
         public void SetVelocity(double velocityNumber)
         {
             m_Node.NodeObject.Motion.MoveVelStart(velocityNumber);
@@ -80,11 +93,13 @@ namespace Model
             m_IsLocked = true;
         }
 
+        // débloque le moteur
         public void Unlock()
         {
             m_IsLocked = false;
         }
 
+        //
         public void RecordFalse() { m_Node.NodeObject.EnableReq(false); }
 
         public void RecordTrue() { m_Node.NodeObject.EnableReq(true); }
@@ -106,11 +121,13 @@ namespace Model
             }
         }
 
+        // retourne à la position initialie
         public void GoHome()
         {
             m_positionValue = m_Node.PositionValue;
         }
 
+        // met à jour les tout les parametres
         public void ClearInfo()
         {
             m_torquesList.Clear();
@@ -128,6 +145,8 @@ namespace Model
             m_IsDecelerate = false;
             return m_constantes.ACCERATION_MODEL_VMAX / (1 + Math.Exp(m_constantes.ACCELERATION_MODEL_SLOPE * (m_constantes.ACCELERATION_MODEL_TORQUE_SENSITIVITY * TorqueAverage * VelocityAverage - m_constantes.ACCELERATION_MODEL_TOQUE_FLEXION_POINT)));
         }
+
+        // parametre la position
         public void SetPositon(int target, bool absolute)
         {
             m_Node.NodeObject.Motion.MovePosnStart(target, absolute, false);
@@ -139,13 +158,27 @@ namespace Model
             double decel = -m_constantes.DECELERATION_MODEL_SLOPE * ((Math.Pow(VelocityAverage - m_constantes.DECELERATION_MODEL_SHIFT, 2)) / (m_constantes.DECELERATION_MODEL_WIDTH)) + m_constantes.DECELERATION_MODEL_VMAX - TorqueAverage * m_constantes.DECELERATION_MODEL_DAMPING_COEFFICIENT;
             return decel < 0 ? 0 : (decel > 80 ? 80 : decel);
         }
+
+        // Vérifie si le sens change ou non
         public bool Changeway(double vite)
         {
-            if (vite<0 ) 
+            if (vite < 0)
             {
                 return true; }
-            else 
-                { return false; }
+            else
+            { return false; }
+        }
+
+        public double Calibrate()
+
+        {   
+            for (int j = 0; j<itteration2; j++){
+
+                SetVelocity(1);
+                m_torquescalibrationList.Add(this.TorqueAverage);
+                }
+            return m_torquescalibrationList.Max();
+
         }
     }
 }
