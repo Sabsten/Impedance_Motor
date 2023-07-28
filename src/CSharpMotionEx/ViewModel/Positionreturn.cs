c12538c8b995944a06e4431928bbd1b8fe5e3f73
djilaliouadah <djilali.ouadah@viacesi.fr>
2023-07-28 14:50:45 +0300

    dji

--- src/CSharpMotionEx/ViewModel/Positionreturn.cs
+++ src/CSharpMotionEx/ViewModel/Positionreturn.cs
@@ -8,31 +8,111 @@ namespace ViewModel
     {
         public PositionReturn(Motor m)
         {
-            double previousTorque = 0;
-            while (!Console.KeyAvailable)
-            {
-                m.RefreshInfo(30);
-                Console.WriteLine("Average Velocity : " + m.VelocityAverage);
-                Console.WriteLine("Average Torque : " + m.TorqueAverage);
+            m.Disable();
+            Console.WriteLine("Please set the initial position of the motor. Then press any key to begin.");
+            Console.ReadKey();
+            m.ResetPositionToHome();
+            m.Enable();
 
-                if (m.TorqueAverage != 0 && m.VelocityAverage != 0)
+            double torqueMoy = 0;
+            double velocityMoy = 0;
+            double positionMoy = 0;
+            double posObj = 0;
+            double posDiff = 0;
+            while (true)
+            {
+                m.RefreshInfo(10);
+                torqueMoy = m.TorqueAverage;
+                velocityMoy = m.VelocityAverage;
+                positionMoy = m.PositionAverage;
+                //myNodes[n].Motion.MovePosnStart(0, false, false);
+                posObj = m.PositionDependingOnTorque(Math.Abs(torqueMoy));
+
+
+                double diff = 0;
+                if (positionMoy < 0)
                 {
-                    if (previousTorque - m.TorqueAverage >= 0.13)
+                    diff = posObj - positionMoy;
+                }
+                else if (positionMoy > 0)
+                {
+                    diff = positionMoy + posObj;
+                }
+                if (diff < 0)
+                {
+                    diff = 0;
+                }
+
+                double estimation_vitesse_back = m.VelocityDependingOnPositionBackward(diff);
+
+                double estimation_vitesse_forth = m.VelocityDependingOnPositionForward(Math.Abs(positionMoy));
+
+                Console.WriteLine(torqueMoy);
+
+                //Pour les cas ou on est proche de 0 et on applique pas de force
+                if (-60 < posObj && posObj < 60 && Math.Abs(torqueMoy)<1)
+                {
+                    if (positionMoy > 30)
                     {
-                        m.SetVelocity(m.AccelerationModel());
+                        posDiff = -estimation_vitesse_back;
+                    }
+                    else if (positionMoy < -30)
+                    {
+                        posDiff = estimation_vitesse_back;
                     }
                     else
                     {
-                        m.SetVelocity(m.DecelerationModel());
+                        posDiff = 0;
                     }
-                    previousTorque = m.TorqueAverage;
+                }
+                //Va position positive
+                else if (posObj > 60 && torqueMoy < -1)
+                {
+                    //Pour avancer
+                    //Stop si trop loin
+                    if (Math.Abs(positionMoy) > 3500)
+                    {
+                        posDiff = 0;
+                    }
+                    else
+                    {
+                        posDiff = estimation_vitesse_forth;
+                    }
+                }
+                //Va position négative
+                else if (posObj > 60 && torqueMoy > 1)
+                {
+                    //Pour avancer
+                    //Stop si trop loin
+                    if (Math.Abs(positionMoy) > 3500)
+                    {
+                        posDiff = 0;
+                    }
+                    else
+                    {
+                        posDiff = -estimation_vitesse_forth;
+                    }
+                    Console.WriteLine("Vitesse estimée 2:" + estimation_vitesse_forth);
+                }
+                else
+                {
+                    posDiff = 0;
                 }
 
+
+                Console.WriteLine("Position à atteindre: " + posObj);
+                Console.WriteLine("Position moy: " + positionMoy);
+                Console.WriteLine("Force moyenne" + torqueMoy);
+                Console.WriteLine("Déplacement" + posDiff);
+
+                Console.WriteLine("///////////");
+
+                m.SetVelocity(posDiff);
+                m.Wait(100);
             }
 
-            m.Lock(cliNodeStopCodes.STOP_TYPE_DISABLE_RAMP);
-            m.WaitUntilMoveDone(500);
-            Console.ReadKey(true);
+
+
             ErrorAndQuit("Fin program");
         }
 
