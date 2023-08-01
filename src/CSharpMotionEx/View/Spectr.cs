23333a07752a6b0eb48ebee1852de1cc668ead4a
Wooulf <corentin33boucard@gmail.com>
2023-08-01 11:57:40 +0300

    Resizing working with prompts

--- src/CSharpMotionEx/View/Spectr.cs
+++ src/CSharpMotionEx/View/Spectr.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Xml;
 using Model;
 using Newtonsoft.Json;
 using Spectre.Console;
+using static ViewModel.Interface;
 
 namespace ViewModel
 {
@@ -20,61 +23,128 @@ namespace ViewModel
         public string NumMotorRequired { get { return m_NumMotorRequired; } }
         private List<Motor> m_motors;
         public List<Motor> motors { get { return m_motors; } }
+        private volatile bool updateNeeded = false;
 
         public void ModMenu()
         {
             m_ModeListObjectt = ParseJson(GetModsDirectory());
             int lastWindowWidth = 0;
 
+            // Créer une nouvelle tâche pour lire les entrées de l'utilisateur
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        var keyInfo = Console.ReadKey(true);
+                        if (keyInfo.Key == ConsoleKey.Enter)
+                        {
+                            ModeSelection();
+                            MotorSelection();
+                            updateNeeded = true;
+                        }
+                    }
+                    System.Threading.Thread.Sleep(100);
+                }
+            });
+
             while (true)
             {
                 int windowWidth = Console.WindowWidth;
 
-                if (windowWidth != lastWindowWidth)
+                // Si la fenêtre est redimensionnée ou qu'une sélection a été faite, mettez à jour l'affichage
+                if (windowWidth != lastWindowWidth || updateNeeded)
                 {
-                    AnsiConsole.Clear();
-
-                    AnsiConsole.Write(
-                        new FigletText("Impedance-Motor")
-                            .Color(Color.Red));
-
-                    int tableWidth = (int)(windowWidth * 0.8);
-
-                    var table = new Table()
-                        .Width(tableWidth)
-                        .BorderColor(Color.Red)
-                        .AddColumn(new TableColumn("[u]Num[/]").Centered())
-                        .AddColumn(new TableColumn("[u]Name[/]").Centered())
-                        .AddColumn(new TableColumn("[u]Explaination[/]").Centered());
-                    m_ModeList = new List<string>();
-
-                    foreach (var mod in m_ModeListObjectt)
-                    {
-                        table.AddRow(new Text(mod.Number.ToString()).Centered(),
-                                     new Text(mod.Name).Centered(),
-                                     new Text(mod.Explaination).Centered());
-                        m_ModeList.Add(mod.Number + " - " + mod.Name);
-                        table.AddRow("", "", "");
-                    }
-
-                    AnsiConsole.Write(
-                            new Panel(table)
-                                .Expand()
-                                .BorderColor(Color.Red)
-                                .Header("[[ [bold underline blue on white]List of Mods[/] ]]"));
-
+                    DisplayMenu(windowWidth);
                     lastWindowWidth = windowWidth;
+                    updateNeeded = false;
                 }
-
                 System.Threading.Thread.Sleep(500);
+            }
+        }
 
-                if (Console.KeyAvailable)
+        // Les mêmes méthodes que votre code original...
+
+    private void DisplayMenu(int windowWidth)
+        {
+            AnsiConsole.Clear();
+
+            AnsiConsole.Write(
+                new FigletText("Impedance-Motor")
+                    .Color(Color.Red));
+
+            int tableWidth = (int)(windowWidth * 0.8);
+
+            var table = new Table()
+                .Width(tableWidth)
+                .BorderColor(Color.Red)
+                .AddColumn(new TableColumn("[u]Num[/]").Centered())
+                .AddColumn(new TableColumn("[u]Name[/]").Centered())
+                .AddColumn(new TableColumn("[u]Explaination[/]").Centered());
+
+            m_ModeList = new List<string>();
+
+            foreach (var mod in m_ModeListObjectt)
+            {
+                table.AddRow(new Text(mod.Number.ToString()).Centered(),
+                             new Text(mod.Name).Centered(),
+                             new Text(mod.Explaination).Centered());
+                m_ModeList.Add(mod.Number + " - " + mod.Name);
+                table.AddRow("", "", "");
+            }
+
+            AnsiConsole.Write(
+                    new Panel(table)
+                        .Expand()
+                        .BorderColor(Color.Red)
+                        .Header("[[ [bold underline blue on white]List of Mods[/] ]]"));
+        }
+
+        // Le reste de votre code...
+
+        public class InputReader
+        {
+            private Queue<ConsoleKeyInfo> keyBuffer = new Queue<ConsoleKeyInfo>();
+            private Thread inputThread;
+            private AutoResetEvent getInput, gotInput;
+            public bool KeyAvailable { get { lock (keyBuffer) return keyBuffer.Count > 0; } }
+
+            public InputReader()
+            {
+                getInput = new AutoResetEvent(false);
+                gotInput = new AutoResetEvent(false);
+                inputThread = new Thread(reader);
+                inputThread.IsBackground = true;
+                inputThread.Start();
+            }
+
+            public void StartReading()
+            {
+                if (inputThread.IsAlive) return;
+                inputThread.Start();
+            }
+
+            private void reader()
+            {
+                while (true)
                 {
-                    var keyInfo = Console.ReadKey(true);
-                    if (keyInfo.Key == ConsoleKey.Enter)
+                    getInput.WaitOne();
+                    while (Console.KeyAvailable)
                     {
-                        break;
+                        lock (keyBuffer) keyBuffer.Enqueue(Console.ReadKey(true));
                     }
+                    gotInput.Set();
+                }
+            }
+
+            public ConsoleKeyInfo ReadKey()
+            {
+                getInput.Set();
+                gotInput.WaitOne();
+                lock (keyBuffer)
+                {
+                    return keyBuffer.Dequeue();
                 }
             }
         }
